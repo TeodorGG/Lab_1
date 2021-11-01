f45728bf7987ca5211019e2bcad7a22dcfae1153
Tudor Deviza <tudor.devizamd@gmail.com>
2021-11-01 14:25:40 +0200

    Beuti

--- js/alert_dialog.js
+++ js/alert_dialog.js
@@ -1,38 +1,37 @@
 function image(src) {
-    
-
-  $(document).ready(function(){
+    $(document).ready(function () {
         $(".dialog_box").css("display", "flex");
-        $(".dialog_box img").attr("src",src);
-  });
-  
+        $(".dialog_box img").attr("src", src);
+    });
 }
 
 
-// Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
 }
 
 var slideIndex = 1;
